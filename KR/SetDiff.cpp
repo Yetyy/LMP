aca03f38d91b222f035f6e8bd25c029cb524ce1c
Yetyy <erohin20144@gmail.com>
2023-03-23 22:51:23 +0300

    Create SetDiff.cpp

---
+++ KR/SetDiff.cpp
@@ -0,0 +1,56 @@
+//1.(set) »з текста где не опреденое кол - во пробелов, 
+//надо найти элементы который повтор€ютс€.
+//—делать функцию дл€ этого и передать в нее л€мбду,
+//котора€ еще провер€ла бы слова на то что у них все буквы разные.
+//¬ итоге найти слова которые повтор€ютс€ и не имеют повтор€ющихс€ букв
+#include <set>
+#include <iostream>
+#include <fstream>
+#include <functional>
+
+auto check = [](std::string word)
+{
+    std::set<char> s;
+    int i=0;
+    while (i < word.size() && s.find(word[i]) == s.end())
+    {
+        s.insert(word[i]);
+        i++;
+    }
+    return i == word.size();
+};
+
+void print(std::set<std::string>);
+std::set<std::string> Task(std::string, std::function<bool(std::string)>);
+
+int main()
+{
+    std::string file_name("test.txt");
+    std::set<std::string> S = Task(file_name, check);
+    print(S);
+    return 0;
+}
+
+std::set<std::string> Task(std::string file_name, std::function<bool(std::string)> comp)
+{
+    std::ifstream file(file_name);
+    std::set<std::string> tmp;
+    std::set<std::string> result;
+    std::string word;
+    while (file >> word)
+    {
+        if (comp(word))
+            if (tmp.find(word) != tmp.end())
+                result.insert(word);
+            else
+                tmp.insert(word);
+    }
+    file.close();
+    return result;
+}
+
+void print(std::set<std::string> S)
+{
+    for (auto i : S)
+        std::cout << i << ' ';
+}
